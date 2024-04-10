d3cf063056efcc305fc43b9cc8fb04974295bbfc
aki029 <kodachi.akihiko.029@outlook.jp>
2024-04-10 12:18:26 +0900

    add X logo

--- common/sidebar.php
+++ common/sidebar.php
@@ -33,13 +33,16 @@
                         <a href="/info/">今年度開催情報</a>
                         <a href="/contact/">お問い合わせ</a>
                     </div>
+                    <div class="SNS">
+                        <a href='https://twitter.com/apuchofusai' target='_blank' rel='noreferrer noopener'><img src='/app/common/image/sidebar/logo-white.png'></a>
+                    </div>
+                    <div class="toHP">
+                        <a href="https://www.akita-pu.ac.jp/gakusei/chofusai/" target="_blank" rel="noreferrer noopener" title="新規タブを開きます">公式HPへ</a>
+                    </div>
                     <div class="adds">
                         <a href="/app/apply/sponsor"><img src="/app/common/image/sidebar/ad_sponsor.jpg"><span>協賛企業様を募集しております！</span></a>
                         <a href="/app/apply/club"><img src="/app/common/image/sidebar/ad_club.jpeg"><span>学外の方も模擬店出店募集中！</span></a>
                         <a href="/app/apply/market"><img src="/app/common/image/sidebar/ad_market.jpeg"><span>今年度よりフリーマーケット開催！</a>
-                    </div>
-                    <div class="toHP">
-                        <a href="https://www.akita-pu.ac.jp/gakusei/chofusai/" target="_blank" rel="noreferrer noopener" title="新規タブを開きます">公式HPへ</a>
                     </div>
                 </div>
             </nav>
