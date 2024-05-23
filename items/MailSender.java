aa5225a831710bab11a4d06af122c6389e223162
kodachi <kodachi.akihko@gmail.com>
2024-05-23 19:12:34 +0900

    mailregist

---
+++ items/MailSender.java
@@ -0,0 +1,58 @@
+import java.io.FileReader;
+import java.util.Properties;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import javax.mail.MessagingException;
+
+import javax.mail.Session;
+import javax.mail.Message;
+import javax.mail.Transport;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.InternetAddress;
+
+public class MailSender {
+
+	public static void main(String[] args) throws IOException, FileNotFoundException{
+		
+		// プロパティファイルから認証に使用するデータを取得
+		Properties prop = new Properties();
+		prop.load(new FileReader("src/setting/mail.properties"));
+		
+		// 送信元のGmailアドレス
+		final String username = prop.getProperty("mailaddress");
+		// Gmailのアカウントのアプリパスワード
+		final String password = prop.getProperty("password");
+
+		// SMTPサーバへの認証とメールセッションの作成 
+		// ※メールセッション = メールの送信に関するパラメータや設定を保持
+		Session session = Session.getInstance(prop, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
+        
+		try {
+			// メール送信準備
+			Message message = new MimeMessage(session);
+			// 送信元の設定
+			message.setFrom(new InternetAddress(username));
+			// 送信先の設定
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(username));
+			// 件名の設定
+			message.setSubject("JavaMail Test");
+			// 本文の設定
+			message.setText("This is a test email sent from JavaMail.");
+
+			// メールの送信
+			Transport.send(message);
+			// 成功時のメッセージ
+			System.out.println("Email sent successfully.");
+		} catch (MessagingException e) {
+			// 失敗時のメッセージ
+			System.err.println("Email sent unsuccessfully ： " + e );
+		}
+	}
+}
