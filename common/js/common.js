e4054df5c51c12f736d9b9c49fe533f06af8097b
kodachi <kodachi.akihko@gmail.com>
2024-06-05 21:37:51 +0900

    fix largefile pushing error

--- common/js/common.js
+++ common/js/common.js
@@ -1,1 +1,12 @@
-$('input#toggle').
+window.onload = function(){
+    $("input#toggle").on("click",function(){
+        $("html").toggleClass('hamburgered');
+    })
+    $(document).on("click",function(event){
+        if(!$(event.target).closest("div.hamburgur-menu").length){
+            $("html").removeClass('hamburgered');
+            $("input#toggle").prop('checked',false);
+            console.log('clicked');
+        }
+    })
+}
